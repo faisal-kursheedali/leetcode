10265499827ffcccfda2744ffdde4a36713e5975
FAISAL <85350108+faisal-kursheedali@users.noreply.github.com>
2026-07-08 09:23:30 +0530

    Time: 0 ms (100.00%) | Memory: 54 MB (25.54%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -4,14 +4,17 @@
  * @return {number}
  */
 var removeElement = function(nums, val) {
-    let k = 0
-    for (let i in nums){
-        if (nums[i]!=val){
-            nums[k] = nums[i]
-            k+=1
-        }
+    // let k = 0
+    // for (let i in nums){
+    //     if (nums[i]!=val){
+    //         nums[k] = nums[i]
+    //         k+=1
+    //     }
+    // }
+    // return k
+    const filteredArr = nums.filter((e)=> e!= val);
+    for(i in nums){
+        nums[i] = filteredArr[i]
     }
-    console.log(k)
-    console.log(nums)
-    return k
+    return filteredArr.length;
 };
