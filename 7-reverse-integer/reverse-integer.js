fccad2c8b5234ae6da3e549ad9cc714577449419
FAISAL <85350108+faisal-kursheedali@users.noreply.github.com>
2026-07-14 06:36:51 +0530

    Time: 50 ms (66.33%) | Memory: 55.3 MB (84.23%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -6,15 +6,20 @@ var reverse = function(x) {
     if(x < -2147483648 || x > 2147483647){
         return 0
     }
-    num=0;
     nx = x<0?x * (-1):x
-    while (nx>0){
-        s = (""+nx).length -1
-        n = nx%10;
-        num += n*(10**s)
-        nx = Math.floor(nx/10);
-    }
-    num = x<0 ? num*(-1) : num;
+
+    // 1st way
+    // num=0;
+    // while (nx>0){
+    //     s = (""+nx).length -1
+    //     n = nx%10;
+    //     num += n*(10**s)
+    //     nx = Math.floor(nx/10);
+    // }
+    // num = x<0 ? num*(-1) : num;
+    // 2nd way
+    num = Number((""+nx).split("").reverse().join(""));
+    num = x<0? num*-1 : num;
     if(num < -2147483648 || num > 2147483647){
         return 0
     }
