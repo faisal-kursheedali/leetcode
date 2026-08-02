c094f6077f9756758ce253c6d528cfb7a5185ed0
FAISAL <85350108+faisal-kursheedali@users.noreply.github.com>
2026-08-02 08:01:35 +0530

    Time: 3 ms (37.37%) | Memory: 53.8 MB (58.59%) - LeetSync

---
+++ 909-stone-game/stone-game.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} piles
+ * @return {boolean}
+ */
+var stoneGame = function(piles) {
+    alice = 0;
+    bob = 0;
+    for(let i = 1; i< piles.length; i++){
+        pile1 = piles[i-1];
+        pile2 = piles[piles.length-i];
+        if(pile1> pile2){
+            alice = alice + pile1;
+            bob = bob + pile2
+        }else{
+            alice = alice + pile2
+            bob = bob + pile1
+        }
+        if(i == Math.floor(piles.length-1/2)){
+            break;
+        }
+    }
+    console.log(alice)
+    console.log(bob)
+    return alice>bob
+};
